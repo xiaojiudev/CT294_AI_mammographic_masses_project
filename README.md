65bb971fb18e7fa80c2ac530aa1aa068af52595e
ldphat99 <ldphat99@gmail.com>
2023-10-31 23:09:02 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+```shell
+                    count       mean        std   min   25%   50%   75%   max
+BI-RADS assessment  959.0   4.348279   1.783031   0.0   4.0   4.0   5.0  55.0
+Age                 956.0  55.487448  14.480131  18.0  45.0  57.0  66.0  96.0
+Shape               930.0   2.721505   1.242792   1.0   2.0   3.0   4.0   4.0
+Margin              913.0   2.796276   1.566546   1.0   1.0   3.0   4.0   5.0
+Density             885.0   2.910734   0.380444   1.0   3.0   3.0   3.0   4.0
+Severity            961.0   0.463059   0.498893   0.0   0.0   0.0   1.0   1.0
+```
+
+* count field: The number of non-null values.
+* mean (Average): The average from all the values of that attribute and dividing by the count of non-null values.
+* std (Standard Deviation): The standard deviation measures the dispersion or spread of the data. It tells how much individual data points deviate from the mean. A higher standard deviation indicates that the data points are more spread out from the mean, while a lower standard deviation suggests that the data points are closer to the mean.
+* min: The smallest value in the dataset
+* 25%: 
+* 50%:
+* 75%:
+* max: The largest value in the dataset
---
+++ mammographicMasses.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Define column names
+column_names = [
+    "BI-RADS",
+    "Age",
+    "Shape",
+    "Margin",
+    "Density",
+    "Severity"
+]
+
+# Load the dataset
+file_path = "mammographic_masses.data.txt"
+dataset = pd.read_csv(file_path, na_values=['?'], names=column_names, delimiter=",")
+
+
+# Display the first 3 rows in dataset
+# print(dataset.head(3))
+
+# Display dataset to evaluate whether data needs cleaning
+# print(dataset.describe().transpose())
+# print(dataset['Severity'].value_counts())
+
+# Display percentage of missing value
+def percent_missing(df):
+    percent_null = 100 * df.isnull().sum() / len(df)
+    percent_null = percent_null[percent_null > 0].sort_values()
+    return percent_null
+
+
+percent_result = percent_missing(dataset)
+sns.barplot(x=percent_result.index, y=percent_result)
+plt.xlabel('Attributes')
+plt.ylabel('% Missing')
+plt.title('Percentage of Missing Values by Attribute')
+
+# plt.show()
+
+# Display all rows that having NaN value => 130 rows
+# print(dataset[(dataset['Age'].isnull()) |
+#               (dataset['Shape'].isnull()) |
+#               (dataset['Margin'].isnull()) |
+#               (dataset['Density'].isnull())])
+
+# Display detail total NaN for each attribute
+# print(dataset.isnull().sum())
+
+# Data seems randomly distributed, so drop all rows having missing value
+new_dataset = dataset.dropna()
+
+# New dataset with 830 rows
+# print(new_dataset)
+
+# Check new dataset is having missing value?
+# print(new_dataset.isnull().sum())
+
+attributes = new_dataset.drop(['Severity', 'BI-RADS'], axis=1)
+label = new_dataset['Severity']
+# print(attributes)
+# print(label)
+
+X_train, X_test, y_train, y_test = train_test_split(attributes, label, test_size=0.25, random_state=100)
+
+scaler = StandardScaler()
+scaler_X_train = scaler.fit_transform(X_train)
+scaler_X_test = scaler.transform(X_test)
+
